5a4bac75759e4011bdeb560b2d0b737d5d6a433f
vj18 <vibhorjain13@gmail.com>
2015-01-23 14:57:11 +0530

    Modified cacheMatrix.R, both functions working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions provide a diffetent way to save a matrix and
+## its inverse as a single R object. Both the functions check for NULL values before working
+## on the input R object.
 
-## Write a short comment describing this function
+## The makeCacheMatrix accepts a matrix and defines four functions inside. It returns
+## a list of those four functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        if(is.null(x)){
+                message("Wrong Input!\nReturning NULL")
+                return(NULL)
+        }
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverse) inv <<- inverse
+        getInv <- function() inv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function checks if the inverse has already been calculated, if yes then it 
+## returns the inverse and if not then it calculates the inverse, updates the object with the
+## calculated inverse and returns the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+        if(is.null(x)){
+                message("x is not an acceptable R object\nReturning NULL")
+                return(NULL)
+        }
+        inv <- x$getInv()
+        if(!is.null(inv)) {
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(x$get())
+        x$setInv(inv)
+        inv
 }
